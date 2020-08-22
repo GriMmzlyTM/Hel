c9df875f6f8ca2e660eaeb54f596e44dc16d1dde
GrimmzlyTM <lorenzo@tortonmind.com>
2020-08-22 12:09:34 -0400

    Add invalid component exception

--- Hel.Engine/ECS/Components/ComponentDictionary.cs
+++ Hel.Engine/ECS/Components/ComponentDictionary.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Hel.Engine.ECS.Exceptions;
 
 namespace Hel.Engine.ECS.Components
 {
---
+++ Hel.Engine/ECS/Exceptions/InvalidComponentException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hel.Engine.ECS.Exceptions
+{
+    public class InvalidComponentException : Exception
+    {
+        public  InvalidComponentException() {}
+        public InvalidComponentException(string msg) : base(msg) {}
+    }
+}
