e1bd7e1c3f635b0a33a21221f89d3dca33101f3a
GrimmzlyTM <lorenzo@tortonmind.com>
2020-08-23 18:15:05 -0400

    Core tiled

---
+++ Hel.Tiled/Models/Property.cs
@@ -0,0 +1,9 @@
+﻿namespace Hel.Tiled.Models
+{
+    public class Property
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public string Value { get; }
+    }
+}
---
+++ Hel.Tiled/Models/Template.cs
@@ -0,0 +1,13 @@
+﻿using Hel.Tiled.Models.Layers.Objects;
+
+namespace Hel.Tiled.Models
+{
+    public class Template
+    {
+        public string Type { get; }
+        
+        public Tileset.Tileset? Tileset { get; }
+        
+        public Object Object { get; }
+    }
+}
