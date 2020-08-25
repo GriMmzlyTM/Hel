7998b9d9b4367b3e0cb5b4887cc5f8abb3c09e27
GrimmzlyTM <lorenzo@tortonmind.com>
2020-08-24 21:20:48 -0400

    Cleanup Tiled

--- Hel.Tiled/Models/Tileset/Tileset.cs
+++ Hel.Tiled/Models/Tileset/Tileset.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 using Hel.Tiled.Models.Enums;
 using Hel.Tiled.Models.Tileset.Tiles;
 using Hel.Tiled.Models.Tileset.Wang;
+using Newtonsoft.Json;
 
 namespace Hel.Tiled.Models.Tileset
 {
     public class Tileset
     {
+
         /// <summary>
-        /// 	Name given to this tileset
+        /// Name given to this tileset
         /// </summary>
         public string Name { get; set; }
         
@@ -16,15 +19,7 @@ namespace Hel.Tiled.Models.Tileset
         /// The number of tile columns in the tileset
         /// </summary>
         public int Columns { get; set; }
-        
-        /// <summary>
-        /// Each tileset has a firstgid (first global ID) property which tells you the global ID of its first tile
-        /// (the one with local tile ID 0). This allows you to map the global IDs back to the right tileset,
-        /// and then calculate the local tile ID by subtracting the firstgid from the global tile ID. The first tileset
-        /// always has a firstgid value of 1.
-        /// </summary>
-        public int FirstGid { get; set; }
-        
+
         /// <summary>
         /// Optional
         /// </summary>
@@ -64,11 +59,6 @@ namespace Hel.Tiled.Models.Tileset
         /// Array of custom properties
         /// </summary>
         public List<Property> Properties { get; set; }
-        
-        /// <summary>
-        /// The external file that contains this tilesets data
-        /// </summary>
-        public string Source { get; set; }
         
         /// <summary>
         /// Spacing between adjacent tiles in image (pixels)
