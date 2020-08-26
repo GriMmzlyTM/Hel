9b8f39ec187e2417dd267adaa5abf5afd1884b13
GrimmzlyTM <lorenzo@tortonmind.com>
2020-08-26 19:23:45 -0400

    cleanup component dict

--- Hel.Engine/ECS/Components/ComponentDictionary.cs
+++ Hel.Engine/ECS/Components/ComponentDictionary.cs
@@ -5,20 +5,12 @@ using Hel.Engine.ECS.Exceptions;
 
 namespace Hel.Engine.ECS.Components
 {
-    public class ComponentDictionary : IDictionary<Type, IComponent>
+    public class ComponentDictionary : Dictionary<Type, IComponent>
     {
 
-        private readonly Dictionary<Type, IComponent> _components;
+        public ComponentDictionary() : base() { }
 
-        public ComponentDictionary()
-        {
-            _components = new Dictionary<Type, IComponent>();
-        }
-
-        public ComponentDictionary(Dictionary<Type, IComponent> newComponents)
-        {
-            _components = new Dictionary<Type, IComponent>(newComponents);
-        }
+        public ComponentDictionary(IDictionary<Type, IComponent> newComponents) : base(newComponents) { }
 
         public T GetComponentOfType<T>() where T : IComponent
         {
@@ -48,10 +40,11 @@ namespace Hel.Engine.ECS.Components
         public void UpdateComponent(IComponent component)
         {
             Type componentType = component.GetType();
-            if (ContainsKey(componentType))
-                _components[componentType] = component;
-            else
+            if (ContainsKey(componentType)) {
+                this[componentType] = component;
+            } else {
                 AddByObject(component);
+            }
         }
 
         public void UpdateComponents(ComponentDictionary componentDictionary)
@@ -60,89 +53,13 @@ namespace Hel.Engine.ECS.Components
                 UpdateComponent(component.Value);
         }
 
-        public void AddByObject(IComponent component) =>
-            _components.Add(component.GetType(), component);
+        public void AddByObject(IComponent component) => Add(component.GetType(), component);
 
-        public void AddByType<T>() where T : Type, IComponent, new() =>
-            _components.Add(typeof(T), new T());
+        public void AddByType<T>() where T : Type, IComponent, new() => Add(typeof(T), new T());
 
-        public void RemoveByType<T>() where T : Type, IComponent =>
-            _components.Remove(typeof(T));
+        public void RemoveByType<T>() where T : Type, IComponent => Remove(typeof(T));
+        
 
-        public IEnumerator<KeyValuePair<Type, IComponent>> GetEnumerator()
-        {
-            foreach (var component in _components)
-            {
-                yield return component;
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        // IDictionary implementation
-
-        public ICollection<Type> Keys => ((IDictionary<Type, IComponent>)_components).Keys;
-
-        public ICollection<IComponent> Values => ((IDictionary<Type, IComponent>)_components).Values;
-
-        public int Count => ((IDictionary<Type, IComponent>)_components).Count;
-
-        public bool IsReadOnly => ((IDictionary<Type, IComponent>)_components).IsReadOnly;
-
-        public IComponent this[Type key] { get => ((IDictionary<Type, IComponent>)_components)[key]; set => ((IDictionary<Type, IComponent>)_components)[key] = value; }
-
-        public bool ContainsKey(Type key)
-        {
-            return ((IDictionary<Type, IComponent>)_components).ContainsKey(key);
-        }
-
-        public void Add(Type key, IComponent value)
-        {
-            ((IDictionary<Type, IComponent>)_components).Add(key, value);
-        }
-
-        public bool Remove(Type key)
-        {
-            return ((IDictionary<Type, IComponent>)_components).Remove(key);
-        }
-
-        public bool TryGetValue(Type key, out IComponent value)
-        {
-            return ((IDictionary<Type, IComponent>)_components).TryGetValue(key, out value);
-        }
-
-        public void Add(KeyValuePair<Type, IComponent> item)
-        {
-            ((IDictionary<Type, IComponent>)_components).Add(item);
-        }
-
-        public void Clear()
-        {
-            ((IDictionary<Type, IComponent>)_components).Clear();
-        }
-
-        public bool Contains(KeyValuePair<Type, IComponent> item)
-        {
-            return ((IDictionary<Type, IComponent>)_components).Contains(item);
-        }
-
-        public void CopyTo(KeyValuePair<Type, IComponent>[] array, int arrayIndex)
-        {
-            ((IDictionary<Type, IComponent>)_components).CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(KeyValuePair<Type, IComponent> item)
-        {
-            return ((IDictionary<Type, IComponent>)_components).Remove(item);
-        }
-
-        IEnumerator<KeyValuePair<Type, IComponent>> IEnumerable<KeyValuePair<Type, IComponent>>.GetEnumerator()
-        {
-            return ((IDictionary<Type, IComponent>)_components).GetEnumerator();
-        }
     }
 
 }
