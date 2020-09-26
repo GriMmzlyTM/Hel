bb94a4874d1b3d05de02291499c567ba22c3eaaa
GrimmzlyTM <lorenzo@tortonmind.com>
2020-09-26 17:56:08 -0400

    updated worlds

---
+++ Hel.Engine/ECS/World/World.cs
@@ -0,0 +1,32 @@
+﻿using Hel.Engine.ECS.Entities.Logic;
+using Hel.Engine.ECS.Entities.Matcher;
+using Hel.Engine.ECS.Systems.Logic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Hel.Engine.ECS.World
+{
+    /// <summary>
+    /// Worlds are stored within World Managers and contain their own instance of
+    /// EntityManager, SystemManager and SpriteBatch. 
+    /// </summary>
+    public class World
+    {
+        public string Name { get; private set; }
+        public EntityManager EntityManager;
+        public SystemManager SystemManager;
+        public EntityMatcher EntityMatcher;
+        
+        public SpriteBatch SpriteBatch { get; private set; }
+
+        public bool IsPrimary;
+        public World(Game game, SpriteBatch spriteBatch, string name)
+        {
+            SpriteBatch   = spriteBatch;
+            Name = name;
+            EntityManager = new EntityManager(this);
+            EntityMatcher = new EntityMatcher();
+            SystemManager = new SystemManager(game, this);
+        }
+    }
+}
