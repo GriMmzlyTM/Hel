083086ec20e874a95fc66c7c9f4280213a0c7c18
GrimmzlyTM <lorenzo@tortonmind.com>
2020-09-26 17:56:21 -0400

    Invalid entity group exception

---
+++ Hel.Engine/ECS/Exceptions/InvalidEntityGroupException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hel.Engine.ECS.Exceptions
+{
+    public class InvalidEntityGroupException  : Exception
+    {
+        public  InvalidEntityGroupException() {}
+        public InvalidEntityGroupException(string msg) : base(msg) {}
+    }
+}
