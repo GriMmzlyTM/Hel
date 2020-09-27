34144591fd3d295fe31768b6e9b91513d71f8dbf
GrimmzlyTM <lorenzo@tortonmind.com>
2020-09-27 17:18:21 -0400

    Split ECS and decouple from monogame

---
+++ Hel.ECS/Components/Examples/RenderExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Hel.ECS.Components.Examples
+{
+    /// <summary>
+    /// The IRender component handles rendering the 
+    /// entity texture to the screen. 
+    /// IRender should be used for any entity that contains
+    /// visuals as opposed to simply logic. 
+    /// 
+    /// IRender has its own Render system which is added to the
+    /// SystemManager when the World is created. 
+    /// </summary>
+    public struct RenderExample : IRenderComponentExample, IEquatable<RenderExample>
+    {
+        /// <summary>
+        /// Texture is the Texture2D your entity should draw to the screen 
+        /// </summary>
+        //public Texture2D Texture { get; set; }
+        
+        public ushort ZIndex { get; set; }
+        public bool Active { get; set; }
+        
+
+        public bool Equals(RenderExample other)
+        {
+            return ZIndex == other.ZIndex 
+                   && Active == other.Active;
+        }
+    }
+}
--- Hel.Engine/ECS/Components/Examples/IRenderComponentExample.cs
+++
@@ -1,9 +0,0 @@
-﻿using Hel.Engine.ECS.Components.Model;
-
-namespace Hel.Engine.ECS.Components.Examples
-{
-    public interface IRenderComponentExample : IComponent
-    {
-        public ushort ZIndex { get; set; }
-    }
-}
